45de286860c161b7aa4e5fe5e52fb31173e43668
Ravbug <ravbuganimations@gmail.com>
2020-01-13 12:41:46 -0500

    Add bot invite link as comment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//bot invite link:  https://discordapp.com/oauth2/authorize?client_id=666309507105816586&scope=bot&permissions=8192
+
 const Discord = require('discord.js'); 
 const client = new Discord.Client();  
 client.on('ready', () => {   
